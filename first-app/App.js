47be90d65f908d81a01d0dca0fe07d1d17cfaa55
Nadine Rodriguez <nadine.rodriguez@gmail.com>
2020-01-12 17:56:35 -0500

    course goal

--- first-app/App.js
+++ first-app/App.js
@@ -4,17 +4,22 @@ import { StyleSheet, Text, View,TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{padding:100}}>
-    <TextInput placeholder="Email" style={{borderBottomColor: 'black', borderBottomWidth: 1}} />
-    <Button title="Submit"/>
+      <View style= {{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Course Goal" style={{width: '90%', borderColor: 'black', borderWidth: 1, padding:5}} />
+        <Button title="ADD"/>
+      </View>
+
+      <View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// })
