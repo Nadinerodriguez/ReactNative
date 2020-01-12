76318195396667eb364d25b9a2b9ebf9e47f9ef6
Nadine Rodriguez <nadine.rodriguez@gmail.com>
2020-01-11 23:03:24 -0500

    jwenfej

--- first-app/App.js
+++ first-app/App.js
@@ -1,12 +1,11 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View,TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress = {() => setOutputText('The text changed')}/>
+    <View style={{padding:100}}>
+    <TextInput placeholder="Email" style={{borderBottomColor: 'black', borderBottomWidth: 1}} />
+    <Button title="Submit"/>
     </View>
   );
 }
