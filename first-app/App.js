1884ef49a86862d437ce17e975e98d4b26b11e25
Nadine Rodriguez <nadine.rodriguez@gmail.com>
2020-01-12 19:20:38 -0500

    useState

--- first-app/App.js
+++ first-app/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View,TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+
+  // function goalInputHandler(enteredText) {
+  //   setEnteredGoal(enteredText);
+  // }
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  };
+
   return (
     <View style={styles.screen}>
       <View style= {styles.container}>
-        <TextInput placeholder="Course Goal" style={{width: '90%', borderColor: 'black', borderWidth: 1, padding:5}} />
-        <Button title="ADD"/>
+        <TextInput placeholder="Course Goal" style={styles.textInput} onChangeText={goalInputHandler}
+        value ={enteredGoal}/>
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
     </View>
   );
@@ -20,5 +34,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center'
-  }
+  },
+  textInput: {
+    width: '90%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding:5
+  },
+
 })
